572adafb99a991831280989b1817cc5e80fed7d1
Ryan Nguyen <rln7471@mavs.uta.edu>
2025-01-01 01:38:02 -0600

    able to add accounts to account db

--- workout-app/src/main/java/controllers/AccountController.java
+++ workout-app/src/main/java/controllers/AccountController.java
@@ -3,6 +3,8 @@ package controllers;
 import model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import repositories.AccountRepository;
 
@@ -18,5 +20,12 @@ public class AccountController {
     @GetMapping("/accounts")
     public Iterable<Account> getAccounts(){
         return accountRepository.findAll();
+    }
+
+    @PostMapping("/insert")
+    public void insertAccount(
+            @RequestBody Account account
+    ){
+        accountRepository.insertAccount(account.getName(), account.getPasskey());
     }
 }
--- workout-app/src/main/java/repositories/AccountRepository.java
+++ workout-app/src/main/java/repositories/AccountRepository.java
@@ -1,9 +1,14 @@
 package repositories;
 
 import model.Account;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account,Long> {
+    @Modifying
+    @Query("INSERT INTO account (name,passkey) VALUES (:name,:passkey)")
+    void insertAccount(String name, String passkey);
 }
