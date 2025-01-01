a3d6d49570a626d0aa141923e939be0073501df1
Ryan Nguyen <rln7471@mavs.uta.edu>
2025-01-01 00:40:25 -0600

    initial commit

---
+++ workout-app/src/test/java/com/example/workout_app/WorkoutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workout_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
